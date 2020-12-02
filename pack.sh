0d3fddc6626291bca0ada181d2e049ff8eed99b7
meir@redislabs.com <meir@redislabs.com>
2020-12-02 19:15:08 +0200

    fix packer

--- pack.sh
+++ pack.sh
@@ -1,6 +1,6 @@
 mkdir -p ./artifacts/snapshot
 tar -czvf ./artifacts/snapshot/gears-jvm.linux-$OS-x64.$GIT_BRANCH.tgz \
---transform "s,^./src/,./plugins/," \
+--transform "s,^./src/,./plugins/," --transform "s,^./,/," \
 ./bin/OpenJDK/jdk-11.0.9.1+1/* \
 ./src/gears_jvm.so \
 ./gears_runtime/target/gear_runtime-jar-with-dependencies.jar
