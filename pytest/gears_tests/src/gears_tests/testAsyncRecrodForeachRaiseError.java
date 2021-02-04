34ae8d0c12a711ba21b5fbbc482dd636a608ffb5
meir@redislabs.com <meir@redislabs.com>
2021-02-05 01:23:27 +0200

    Fix tests

--- pytest/gears_tests/src/gears_tests/testAsyncRecrodForeachRaiseError.java
+++ pytest/gears_tests/src/gears_tests/testAsyncRecrodForeachRaiseError.java
@@ -3,13 +3,14 @@ package gears_tests;
 import gears.GearsBuilder;
 import gears.GearsFuture;
 import gears.readers.KeysReader;
+import java.io.Serializable;
 
 public class testAsyncRecrodForeachRaiseError {
 	public static void main() {
 		KeysReader reader = new KeysReader();
 		GearsBuilder.CreateGearsBuilder(reader).map(r->r.getKey()).
 		asyncForeach(r->{
-			GearsFuture<String> f = new GearsFuture<String>();
+			GearsFuture<Serializable> f = new GearsFuture<Serializable>();
 			new Thread(new Runnable() {
 				
 				@Override
